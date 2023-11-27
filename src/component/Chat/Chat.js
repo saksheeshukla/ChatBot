575fc2c5b2a9f6cbe4f90fec120217b5c7ce3db9
Sakshee Shukla <116931171+saksheeshukla@users.noreply.github.com>
2023-11-27 14:48:25 +0530

    Update Chat.js

--- src/component/Chat/Chat.js
+++ src/component/Chat/Chat.js
@@ -9,7 +9,7 @@ import closeIcon from "../../images/closeIcon.png";
 
 let socket;
 
-const ENDPOINT = "http://localhost:4500/";
+const ENDPOINT = "https://letshaveachat-e0u7.onrender.com/";
 
 const Chat = () => {
     const [id, setid] = useState("");
